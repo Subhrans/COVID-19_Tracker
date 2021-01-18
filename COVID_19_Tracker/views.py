773b6b4b4289e85bf2151ed69615862389ddcd5d
subhransu das <subhransud525@gmail.com>
2021-01-18 22:47:47 +0530

    Global Data

--- COVID_19_Tracker/views.py
+++ COVID_19_Tracker/views.py
@@ -1,7 +1,20 @@
+import requests
+import json
 from django.shortcuts import render
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'covid/home.html')
+    r = requests.get('https://api.covid19api.com/summary')
+    summary = r.json()
+    # summary=json.dumps(summary,indent=4, separators=('. ','= '))
+    # summary=dict(summary)
+    global_data=summary['Global']
+    print(summary)
+    print(type(global_data))
+    print(global_data)
+    context={"global_data":global_data,
+             "summary":summary,
+             }
+    return render(request, 'covid/home.html',context)
