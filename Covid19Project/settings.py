5a7bf3ee016c570fee1a7d70549e367a2f3ca22a
subhransu das <subhransud525@gmail.com>
2021-01-23 05:17:13 +0530

    Covid19 app for heroku

--- Covid19Project/settings.py
+++ Covid19Project/settings.py
@@ -108,9 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
